ba22606d1180e3e4bf84be8b58d5b39f8f56212f
tappoy <tappoy1234@gmail.com>
2024-05-07 08:40:03 +0900

    fix using of non buffered channel

--- pwinput.go
+++ pwinput.go
@@ -1,16 +1,25 @@
 // This package provides a robust way to read password from the terminal.
 //
-//   - Read password from the stdin.
-//   - Mute the terminal while inputting the password.
-//   - If interrupted while inputting the password, the terminal will be turned back to normal.
+// Features:
+//	- Read password from the terminal.
+//	- Mute the terminal while inputting the password.
+//	- If interrupted while inputting the password, the terminal will be turned back to normal.
+//	- If the input is not from the terminal, it will read from the Env.In.
+//
+// Dependencies:
+//	 - github.com/tappoy/env (Env.In)
+//	 - golang.org/x/crypto/ssh/terminal
 package pwinput
 
 import (
 	"errors"
+	"io"
 	"os"
 	"os/signal"
 	"syscall"
+	"strings"
 
+	"github.com/tappoy/env"
 	"golang.org/x/crypto/ssh/terminal"
 )
 
@@ -18,50 +27,40 @@ var (
 	// The input process is interrupted.
 	ErrInterrupted = errors.New("ErrInterrupted")
 
-	// The dummy password to generate ErrInterrupted.
-	Interrupt = "Interrupt"
 )
 
 type PasswordInput interface {
 	// InputPassword reads a password from the terminal.
 	// If DummyPasswordInput is used, it will return the dummy password.
 	// Errors:
-	//   - ErrInterrupted
+	//	- ErrInterrupted
 	InputPassword() (string, error)
-}
-
-// Dummy implementation
-type dummyPasswordInput struct {
-	dummyPassword string
-}
-
-// Return the dummy password.
-// Errors:
-//   - ErrInterrupted
-func (pw dummyPasswordInput) InputPassword() (string, error) {
-	if pw.dummyPassword == "Interrupt" {
-		return "", ErrInterrupted
-	} else {
-		return pw.dummyPassword, nil
-	}
-}
-
-// Create a dummy password reader for testing or some other purposes.
-// It will return the dummy password when calling `Inputssword()`.
-//
-// If dummyPassword is "Interrupt" or pwinput.Interrupt,
-// it will return ErrInterrupted when calling `InputPassword()`
-func NewDummyPasswordInput(dummyPassword string) PasswordInput {
-	return dummyPasswordInput{dummyPassword: dummyPassword}
 }
 
 // True implementation
 type passwordInputImpl struct{}
 
+// Create a new password reader.
+func NewPasswordInput() PasswordInput {
+	return passwordInputImpl{}
+}
+
 // InputPassword reads a password from the terminal.
+// If the input is not from the terminal, it will read from the Env.In.
+//
+// Errors:
+//	- ErrInterrupted
 func (pw passwordInputImpl) InputPassword() (string, error) {
+	if terminal.IsTerminal(int(syscall.Stdin)) {
+		return pw.inputFromTerminal()
+	} else {
+		return pw.inputFromReader(env.In)
+	}
+}
+
+func (pw passwordInputImpl) inputFromTerminal() (string, error) {
 	// caputure the signal of Ctrl+C
-	signalChan := make(chan os.Signal)
+	signalChan := make(chan os.Signal, 512)
 	signal.Notify(signalChan, os.Interrupt)
 	defer signal.Stop(signalChan)
 
@@ -103,7 +102,11 @@ func (pw passwordInputImpl) InputPassword() (string, error) {
 	}
 }
 
-// Create a new password reader.
-func NewPasswordInput() PasswordInput {
-	return passwordInputImpl{}
+func (pw passwordInputImpl) inputFromReader(reader io.Reader) (string, error) {
+	env.Outf("input password from reader\n")
+	b, err := io.ReadAll(reader)
+	if err != nil {
+		return "", err
+	}
+	return strings.TrimSpace(string(b)), nil
 }
